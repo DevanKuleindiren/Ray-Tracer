5c24ce3016cab24cc1491fb52d969a3b20688801
Devan Kuleindiren <d.kuleindiren@gmail.com>
2016-01-15 22:04:31 +0000

    Modularise code more in preparation for trace recursion

--- main.cpp
+++ main.cpp
@@ -4,26 +4,44 @@
 #include <float.h>
 #include "IntersectionPoint.h"
 
-Colour getColour (int row, int col) {
-    Colour colour = {row % 255, (row % 255), (row % 255)};
-    return colour;
-}
+const int width   = 500;                                // Width of screen
+const int height  = 500;                                // Height of screen
+const int numberOfObjects = 1;                          // Number of scene objects
+const int numberOfLights = 1;                           // Number of lights
 
-int main() {
+Colour *backgroundColor = new Colour (255, 255, 255);   // White background
+SceneObject *objects[numberOfObjects];                  // Scene objects
+Vector3D<double> *lights[numberOfLights];               // Scene lights
+Colour *pixels[height][width];                          // Screen pixels
 
-    const int width   = 500;        // Width of screen
-    const int height  = 500;        // Height of screen
-    const int numberOfObjects = 1;  // Number of scene objects
-    const int numberOfLights = 1;   // Number of lights
 
-    Colour *backgroundColor = new Colour (255, 255, 255);   // White background
-
-    // Creating an array of objects
-    SceneObject *objects[numberOfObjects];
+void populateScene () {
+    // Add objects
     objects[0] = new Sphere(Vector3D<double>(0, 0, 20), 5, new Colour(255, 0, 0));
 
+    // Add lights
+    lights[0] = new Vector3D<double>(10, 10, 5);
+}
 
-    Colour *pixels[height][width];
+
+Colour *trace (Ray<double> ray) {
+    // Find the closest intersection point
+    IntersectionPoint closestPoint(DBL_MAX, NULL, false);
+    for (SceneObject *object : objects) {
+        IntersectionPoint intersectionPoint = object->nearestIntersection(ray);
+        if (intersectionPoint.isIntersection && intersectionPoint.distance < closestPoint.distance) {
+            closestPoint = intersectionPoint;
+        }
+    }
+
+    if (!closestPoint.isIntersection) return backgroundColor;
+
+    return closestPoint.sceneObject->colour;
+}
+
+
+int main() {
+    populateScene();
 
     for (int row = 0; row < height; row++) {
         for (int col = 0; col < width; col++) {
@@ -31,24 +49,11 @@ int main() {
             Ray<double> rayThroughPixel = Ray<double>(Vector3D<double>(0, 0, 0),
                                       Vector3D<double>(col - (width / 2), row - (height / 2), width / 2));
 
-            IntersectionPoint closestPoint(DBL_MAX, NULL, false);
-            for (SceneObject *object : objects) {
-                IntersectionPoint intersectionPoint = object->nearestIntersection(rayThroughPixel);
-                if (intersectionPoint.isIntersection && intersectionPoint.distance < closestPoint.distance) {
-                    closestPoint = intersectionPoint;
-                }
-            }
-
-            pixels[row][col] = (closestPoint.isIntersection ? closestPoint.sceneObject->colour : backgroundColor);
-            if (row == 250 && col == 250) {
-                std::cout << "A: " << pixels[row][col]->r << ", " << pixels[row][col]->g << ", " << pixels[row][col]->b
-                                   << std::endl;
-            }
+            pixels[row][col] = trace(rayThroughPixel);
         }
     }
 
     // Output image to file
-
     std::ofstream fileStream;
     fileStream.open("/Users/devankuleindiren/Documents/Work/University/Part IB/Computer Graphics and Image Processing/Supervisions/Supervision 4/Ray Tracer/test.ppm");
     fileStream << "P3" << std::endl;
@@ -56,12 +61,6 @@ int main() {
     fileStream << "255" << std::endl;
     for (int row = 0; row < height; row++) {
         for (int col = 0; col < width; col++) {
-
-            if (row == 250 && col == 250) {
-                std::cout << "B: " << pixels[row][col]->r << ", " << pixels[row][col]->g << ", " << pixels[row][col]->b
-                                   << std::endl;
-            }
-
             fileStream << std::setw(4) << pixels[row][col]->r;
             fileStream << std::setw(4) << pixels[row][col]->g;
             fileStream << std::setw(4) << pixels[row][col]->b;
@@ -69,13 +68,13 @@ int main() {
         fileStream << std::endl;
     }
 
-
     // Clear up memory
     delete backgroundColor;
     for (SceneObject *object : objects) {
         delete object;
     }
-
+    
+    // Close the file
     fileStream.close();
     return 0;
 }
