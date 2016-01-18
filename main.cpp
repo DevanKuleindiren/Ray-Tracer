45002a021b5832b83ba6a61505203fc310771d62
Devan Kuleindiren <d.kuleindiren@gmail.com>
2016-01-18 20:20:42 +0000

    Fix fish-bowl view by reducing field of view

--- main.cpp
+++ main.cpp
@@ -5,15 +5,18 @@
 #include "IntersectionPoint.h"
 #include "Light.h"
 
-const int width   = 500;                                                    // Width of screen
-const int height  = 500;                                                    // Height of screen
+const int sceneWidth   = 500;                                               // Width of scene
+const int sceneHeight  = 500;                                               // Height of scene
 const int numberOfObjects = 1;                                              // Number of scene objects
 const int numberOfLights = 1;                                               // Number of lights
+
+const double xFieldOfView = M_PI_4;                                         // x field of view = pi / 2
+const double yFieldOfView = (sceneHeight / sceneWidth) * xFieldOfView;      // y field of view
 
 Vector3D<double> *backgroundColor = new Vector3D<double>(255, 255, 255);    // White background
 SceneObject *objects[numberOfObjects];                                      // Scene objects
 Light *lights[numberOfLights];                                              // Scene lights
-Vector3D<double> *pixels[height][width];                                    // Screen pixels
+Vector3D<double> *pixels[sceneHeight][sceneWidth];                          // Screen pixels
 
 const double ka = 0.3;                                                      // Ambient reflection constant
 const double kd = 0.5;                                                      // Diffuse reflection constant
@@ -22,7 +25,7 @@ const double shininess = 5;                                                 // n
 
 void populateScene () {
     // Add objects
-    objects[0] = new Sphere(Vector3D<double>(0, 0, 20), 5, Vector3D<double>(255, 0, 0));
+    objects[0] = new Sphere(Vector3D<double>(0, 0, 10), 2, Vector3D<double>(255, 0, 0));
 
     // Add lights
     lights[0] = new Light(Vector3D<double>(10, 10, 10), Vector3D<double>(255, 255, 255));
@@ -75,11 +78,19 @@ int main() {
 
     populateScene();
 
-    for (int row = 0; row < height; row++) {
-        for (int col = 0; col < width; col++) {
+    double screenWidth  = 2 * tan(xFieldOfView / 2);
+    double screenHeight = 2 * tan(yFieldOfView / 2);
+    double xStep = screenWidth / sceneWidth;
+    double yStep = screenHeight / sceneHeight;
+    double x0 = -screenWidth / 2;
+    double y0 = -screenHeight / 2;
+
+    for (int row = 0; row < sceneHeight; row++) {
+        for (int col = 0; col < sceneWidth; col++) {
             // Determine RAY
-            Ray<double> rayThroughPixel = Ray<double>(Vector3D<double>(0, 0, 0),
-                                      Vector3D<double>(col - (width / 2), row - (height / 2), width / 2));
+            Ray<double> rayThroughPixel =
+                    Ray<double>(Vector3D<double>(0, 0, 0),
+                                Vector3D<double>(x0 + (col * xStep), y0 + (row * yStep), 1));
 
             pixels[row][col] = trace(rayThroughPixel);
         }
@@ -90,10 +101,10 @@ int main() {
     fileStream.open("/Users/devankuleindiren/Documents/Work/University/Part IB/Computer Graphics and Image "
                             "Processing/Supervisions/Supervision 4/Ray Tracer/scene.ppm");
     fileStream << "P3" << std::endl;
-    fileStream << width << " " << height << std::endl;
+    fileStream << sceneWidth << " " << sceneHeight << std::endl;
     fileStream << "255" << std::endl;
-    for (int row = 0; row < height; row++) {
-        for (int col = 0; col < width; col++) {
+    for (int row = 0; row < sceneHeight; row++) {
+        for (int col = 0; col < sceneWidth; col++) {
             fileStream << std::setw(4) << colourCap(pixels[row][col]->x);
             fileStream << std::setw(4) << colourCap(pixels[row][col]->y);
             fileStream << std::setw(4) << colourCap(pixels[row][col]->z);
